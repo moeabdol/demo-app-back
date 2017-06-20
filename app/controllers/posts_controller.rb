55e9e3624863b8252f4111c6c1164adf51c6157d
moeabdol <admin.r99@gmail.com>
2017-06-20 06:09:50 +0300

    add show action to the posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,18 @@
 class PostsController < ApplicationController
+  before_action :find_post, only: [:show]
+
   def index
     @posts = Post.all
     render json: @posts
   end
+
+  def show
+    render json: @post
+  end
+
+  private
+
+    def find_post
+      @post = Post.find(params[:id])
+    end
 end
